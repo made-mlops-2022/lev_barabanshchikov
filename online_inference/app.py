3e94ae8f7dbee6f49473f6cdc065b03cd0c021c7
Лев Барабанщиков <levbara1@gmail.com>
2022-11-22 16:52:32 +0300

    Added dockerfile && optimized size

--- online_inference/app.py
+++ online_inference/app.py
@@ -79,4 +79,4 @@ def validation_exception_handler(request: Request, exception: RequestValidationE
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
